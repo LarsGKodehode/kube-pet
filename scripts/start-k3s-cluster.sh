94a14d1f9cc494dff34c06dd76daff0f6f950727
larsgkodehode <lars.gunnar.kodehodet@gmail.com>
2023-11-28 02:02:39 +0100

    update cluster startup script

--- scripts/start-k3s-cluster.sh
+++ scripts/start-k3s-cluster.sh
@@ -1,6 +1,9 @@
-k3s_store="./k3s-system"
+k3s_store="/home/zab/projects/kube-pet/.k3s-system"
 
 sudo k3s server  \
-  --data-dir  "$k3s_store/" \
-  --config "$k3s_store/config.yaml" \
-  --write-kubeconfig "$k3s_store/k3s.yaml"
+  --disable=traefik \
+  --data-dir  "${k3s_store}/" \
+  --config "${k3s_store}/config.yaml" \
+  --write-kubeconfig "${k3s_store}/k3s.yaml"
+
+export KUBECONFIG="${k3s_store}k3s.yaml"
